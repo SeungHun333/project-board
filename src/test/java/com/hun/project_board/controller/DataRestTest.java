1277d8b3e59e64d25888e8e2d78ae2cf993d350a
SeungHun333 <yui332@naver.com>
2025-04-24 12:12:00 +0900

    #1 - data rest 테스트
    꼭 필요한 테스트는 아니지만 공부할 겸 추가

---
+++ src/test/java/com/hun/project_board/controller/DataRestTest.java
@@ -0,0 +1,20 @@
+package com.hun.project_board.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void test() {
+      
+    }
+}
